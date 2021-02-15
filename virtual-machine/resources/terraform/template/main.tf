58ffa8e5e47a1c43f76c14815fe221d97cc03696
sgsegura <71374156+sgsegura@users.noreply.github.com>
2021-02-15 11:45:25 +0100

    Update main.tf

--- virtual-machine/resources/terraform/template/main.tf
+++ virtual-machine/resources/terraform/template/main.tf
@@ -1,8 +1,15 @@
 # Specify the provider and access details
 provider "aws" {
-  region = var.aws_region
-  access_key = var.access_key
-  secret_key = var.secret_key
+  region = "eu-west-1"
+  access_key = "test"
+  secret_key = "test"
+  s3_force_path_style = true
+  skip_credentials_validation = true
+  skip_metadata_api_check = true
+  skip_requesting_account_id = true
+  endpoints {
+    vpc = "http://localhost:4566"
+  }
 }
 
 # Create a VPC
